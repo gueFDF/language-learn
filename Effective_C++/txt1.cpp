67d4021fd5436740fa00e203d38eeb58213c1a1b
gueFDF <3237455241@qq.com>
2022-10-11 15:19:40 +0800

    12

--- Effective_C++/txt1.cpp
+++ Effective_C++/txt1.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 #include <string>
-class GamePlear
+#include <cstdio>
+
+class uncopy
+{
+public:
+    uncopy(){};
+
+private:
+    int a;
+    uncopy(const uncopy &);
+    uncopy &operator=(const uncopy &);
+} //当有子类继承他，那么该子类将不允许使用拷贝构造和operator=
+
+;
+class GamePlear : private uncopy
 {
 public:
     static const int NumTurns = 5; //常量申明式
@@ -10,6 +24,7 @@ public:
 
 public:
     GamePlear(std::string p) : ptr(p) {}
+    GamePlear() = default;
     const char &operator[](int i) const
     {
         return ptr[i];
@@ -17,7 +32,7 @@ public:
 
     char operator[](int i)
     {
-        return const_cast<char&>(static_cast<const GamePlear&>(*this)[i]);   //在 no-const operator[]中调用const opeator[]
+        return const_cast<char &>(static_cast<const GamePlear &>(*this)[i]); //在 no-const operator[]中调用const opeator[]
     }
     void print(void) const
     {
@@ -29,13 +44,70 @@ const int GamePlear::NumTurns;
 int main()
 {
     const GamePlear p("abcdefghijklmn");
+
     GamePlear p2("abcdefghijklmn");
     std::cout << p[2] << std::endl;
-    std::cout << p2[2]<<std::endl;
+    std::cout << p2[2] << std::endl;
     p.print();
     std::string str = p.ptr;
+    // const GamePlear c(p);  无法进行拷贝构造
+
+    int q = 3;
+    int m = -15;
+    int &pp = m;
+    printf("%p  %d\n", &q, q);
+    int &pr = q;
+    printf("%p  %d\n", &pr, pr);
+    pr = std::move(pp);
+    printf("%p  %d\n", &pr, pr);
+    //进行更改
+    m = 19;
+    printf("%p  %d\n", &pr, pr);
 }
 
 // 1.对于单纯常量，用const 或 enmu 替换 #define
 // 2.用 inline 内联函数 替换 #define 宏函数
 // const 对象只能访问const修饰的函数
+// C++不允许引用改指向对象
+//可以在praviate中声明拷贝构造，从而可以杜绝用户使用该功能
+
+//析构函数绝对不要吐出异常，如果一个析构函数调用的函数可能抛出异常，析构函数应该捕捉任何异常，然后吞下或接结束程序
+
+//如果客户需要对某个操作函数运行期间抛出的异常做出反应，那么class应该提供一个普通函数（而Fri在析构函数中）执行该操作
+
+//绝不要在构造函数和析构函数中调用virtual函数
+
+//(可以在派生类构造过程中将数据向上传递给基类的构造函数当中，替换)
+
+// 10,另operator=返回一个referance to*this   这样就可以进行  x=y=z=15类似这样的
+
+// 11.在operator=中处理自我赋值   (总会有用户不小心这么做)
+
+//加一个证同测试
+
+/*
+eg:
+    if(dst==&src) return *this;
+    else
+    {
+        delete dst;
+        dst=new  object(src.object)
+    }
+
+
+但是还会存在异常安全问题
+
+改进
+Widget& Widget::operator=(const Widget&rhs)
+{
+    Bitmap *pOrig=bp;
+    pb=new Bitmap(*rhs,pb);
+    delete pOrig;
+    return *this;
+}
+
+
+也可以使用copy-and-swap技术
+*/
+
+//12 .复制对象时勿忘其每一个成分
--- Effective_C++/txt2.cpp
+++ Effective_C++/txt2.cpp
@@ -1,26 +1,86 @@
 // expre_bad_cast_Exception.cpp
 // compile with: /EHsc /GR
-#include <typeinfo>
-#include <iostream>
+// #include <typeinfo>
+// #include <iostream>
 
-class Shape {
-public:
-   virtual void virtualfunc() const {}
-};
+// class Shape {
+// public:
+//    virtual void virtualfunc() const {}
+// };
 
-class Circle: public Shape {
-public:
-   virtual void virtualfunc() const {}
-};
+// class Circle: public Shape {
+// public:
+//    virtual void virtualfunc() const {}
+// };
 
+// using namespace std;
+// int main() {
+//    Shape shape_instance;
+//    Shape& ref_shape = shape_instance;
+//    try {
+//       Circle& ref_circle = dynamic_cast<Circle&>(ref_shape);
+//    }
+//    catch (bad_cast b) {
+//       cout << "Caught: " << b.what();
+//    }
+// }
+
+#include<iostream>
 using namespace std;
-int main() {
-   Shape shape_instance;
-   Shape& ref_shape = shape_instance;
-   try {
-      Circle& ref_circle = dynamic_cast<Circle&>(ref_shape);
+class Calculator
+{
+public:
+	int m_num1;
+	int m_num2;
+   virtual	int getResult()
+   {
+	   return 0;
    }
-   catch (bad_cast b) {
-      cout << "Caught: " << b.what();
-   }
+};
+class Add :public Calculator
+{
+public:
+	virtual int getResult()  override
+	{
+		return m_num1 + m_num2;
+	}
+};
+class Sub :public Calculator 
+{
+public:
+	int getResult()  override
+	{ 
+		return m_num1 - m_num2;	
+	}
+};
+class Mul :public Calculator
+{
+public:
+	int getResult()   override
+	{
+		return m_num1 * m_num2;
+	}
+};
+int main()
+{
+	Calculator* abc;
+	//加法
+	abc = new Add;
+	abc->m_num1 = 100;
+	abc->m_num2 = 100;
+	cout << abc->m_num1 << " + " << abc->m_num2 << " = " << abc->getResult() << endl;
+	delete abc;
+   //减法
+	abc = new Sub;
+	abc->m_num1 = 100;
+	abc->m_num2 = 100;
+	cout << abc->m_num1 << " - " << abc->m_num2 << " = " << abc->getResult() << endl;
+	delete abc;
+	//乘法
+	abc = new Mul;
+	abc->m_num1 = 100;
+	abc->m_num2 = 100;
+	cout << abc->m_num1 << " * " << abc->m_num2 << " = " << abc->getResult() << endl;
+	delete abc;
+	return 0;
 }
