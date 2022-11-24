ae10d8b79316395452425c20bdd09389d8e4bad5
gueFDF <3237455241@qq.com>
2022-11-24 10:34:59 +0800

    朴素协成模拟

--- coroutine/Duff's_Device/main.cc
+++ coroutine/Duff's_Device/main.cc
@@ -1,51 +1,53 @@
 #include <iostream>
 using namespace std;
-
-void opt1()
-{
-    std::cout << 1 << endl;
-}
-void opt2()
-{
-    std::cout << 2 << endl;
-}
-void opt3()
-{
-    std::cout << 3 << endl;
-}
-void opt4()
-{
-    std::cout << 4 << endl;
-}
-
-#define yield(op, x) state = __LINE__; op(); return x;case __LINE__:;
+#define yield()       \
+    state = __LINE__; \
+    return;           \
+    case __LINE__:;
 
 #define begin()           \
     static int state = 0; \
     switch (state)        \
-    {\
-    case 0:\
+    {                     \
+    case 0:
 
 #define end() \
     }
 
-void PrintValue(int value)
+void PrintValue()
 {
     begin();
-    yield(opt1, );
-    yield(opt2, );
-    yield(opt3, );
-    yield(opt4, );
+    cout << " 1 " << endl;
+    yield();
+    cout << " 2 " << endl;
+    yield();
+    cout << " 3 " << endl;
+    yield();
+    cout << " 4 " << endl;
+    end();
+}
+
+void Printnum()
+{
+    begin();
+    cout << " a " << endl;
+    yield();
+    cout << " b " << endl;
+    yield();
+    cout << " c " << endl;
+    yield();
+    cout << " d " << endl;
     end();
 }
 
 int main()
 {
-    int i = 0;
-    for (i = 0; i < 4; i++)
+    int n=4;
+    while (n--)
     {
-        std::cout << "in the " << i << " loop" << std::endl;
-        PrintValue(1);
+        PrintValue();
+        Printnum();
     }
+
     return 0;
 }
